4400f738f2cd9471a04c71c025f07a62c7fe38f0
yamuna aladasu <yamuna.aladasu@bigthinkcode.com>
2026-05-08 12:06:22 +0530

    backend

--- users/views.py
+++ users/views.py
@@ -1,102 +1,75 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from django.contrib.auth.hashers import (
     make_password,
     check_password
 )
-
 from .models import Users
-
 import json
-
-
-# REGISTER API
-
 @csrf_exempt
 def register(request):
-
-    if request.method == "POST":
-
-        data = json.loads(request.body)
-
-        first_name = data.get("first_name")
-        last_name = data.get("last_name")
-        email = data.get("email")
-        password = data.get("password")
-
-        # Check existing email
-
+    if request.method=="POST":
+        data=json.loads(request.body)
+        first_name=data.get("first_name")
+        last_name=data.get("last_name")
+        email=data.get("email")
+        password=data.get("password")
         if Users.objects.filter(email=email).exists():
-
             return JsonResponse({
-                "message": "Email already exists"
-            })
-
-        # Create user
-
-        Users.objects.create(
-
-            first_name=first_name,
-
-            last_name=last_name,
-
-            email=email,
-
-            password=make_password(password)
-        )
-
-        return JsonResponse({
-            "message": "User registered successfully"
+            "message":"email already exists"
         })
-
+        Users.objects.create(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=make_password(password)
+        )
+        return JsonResponse({
+        "message":"registerd successfully"
+         })
     return JsonResponse({
-        "message": "Invalid request"
+        "message":"invalid request"
     })
-
-
-# LOGIN API
-
 @csrf_exempt
 def signin(request):
-
-    if request.method == "POST":
-
-        data = json.loads(request.body)
-
-        email = data.get("email")
-        password = data.get("password")
-
+    if request.method=="POST":
+        data=json.loads(request.body)
+        email=data.get("email")
+        password=data.get("password")
+        if not email or not password:
+            return JsonResponse({
+                "message":"Both the feilds are required"
+            })
         try:
-
-            user = Users.objects.get(email=email)
-
-            # Verify password
-
-            if check_password(password, user.password):
-                
-
-                return JsonResponse({
-
-                    "message": "Login successful",
-
-                    "user": {
-
-                        "first_name": user.first_name,
-                        "last_name": user.last_name,
-                        "email": user.email
-                    }
-                })
-            else:
-                return JsonResponse({
-                    "message": "Invalid password"
-                })
+           user=Users.objects.get(email=email)
+           if check_password(password,user.password):
+               return JsonResponse({
+                "message":"Login Successfull",
+                "user":{
+                    "first_name":user.first_name,
+                    "last_name":user.last_name,
+                    "email":user.email,
+                    
+                }
+            })
+           else:
+               return JsonResponse({
+                "message":"invalid password"
+            })   
         except Users.DoesNotExist:
             return JsonResponse({
-                "message": "User not found"
-            })
+                "message":"user not found"
+
+        })
     return JsonResponse({
-        "message": "Invalid request"
-
-
+        "message":"invalid request"
     })
+    
+        
+
+                         
+        
+
+        
+
+    
